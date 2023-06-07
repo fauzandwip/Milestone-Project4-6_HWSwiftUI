386325f2405bf5c6cc2462be73ab6fd371335ab3
fauzandp <fauzan27dp@gmail.com>
2023-06-07 11:08:48 +0700

    Initial Commit

---
+++ Edutainment/EdutainmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  EdutainmentApp.swift
+//  Edutainment
+//
+//  Created by Fauzan Dwi Prasetyo on 07/06/23.
+//
+
+import SwiftUI
+
+@main
+struct EdutainmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
